2126eeb0d3137197aa67caa5db9ab239cddd9211
wj lee <mymamyma@gmail.com>
2016-06-02 16:12:11 -0400

    regression example file

--- regression.py
+++ regression.py
@@ -1,16 +1,13 @@
-from sklearn import linear_model
-
-clf = linear_model.LinearRegression()
-clf.fit([[0,0],[1,1],[2,2]],[0,1,2])
-
-print clf.coef_
-
 ### Import LinearRegression, create regression
 ### and fit it to the training data
 
 from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
-reg.fit(ages_train, net_worths_train)
 
+x = [[0],[1],[2]]
+y = [1,3,5]
+reg.fit(x,y)
 
-print "r-squared score:", reg.score(ages_test, net_worths_train)
+print "coefficieents:", reg.coef_
+print "intercept:", reg.intercept_
+print "score:", reg.score(x,y)
