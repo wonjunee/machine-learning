e274558bfec8bc0f3ad89b58cc1b0fd4c89a4fa2
wj lee <mymamyma@gmail.com>
2016-06-02 17:28:56 -0400

    delete 10 outliers

--- ud120-projects/outliers/outlier_cleaner.py
+++ ud120-projects/outliers/outlier_cleaner.py
@@ -14,7 +14,14 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    error = abs(predictions - net_worths)
+    max_index = error.argsort(axis=0)[-10:]
+    
+    for i in range(len(ages)):
+        if i not in max_index:
+            cleaned_data.append((ages[i], net_worths[i], error[i]))
 
     
+
     return cleaned_data
 
