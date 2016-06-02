e5e2220a04de5aac02e01cdea2fdae6371161f20
wj lee <mymamyma@gmail.com>
2016-06-02 17:29:44 -0400

    print out coef, intercept, etc after cleaning outliers

--- ud120-projects/outliers/outlier_removal_regression.py
+++ ud120-projects/outliers/outlier_removal_regression.py
@@ -26,15 +26,15 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
 
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
+print "coefficieents:", reg.coef_[0][0]
+print "intercept:", reg.intercept_[0]
+print "score:", reg.score(ages_train, net_worths_train)
+print "score:", reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -46,17 +46,13 @@ plt.show()
 
 ### identify and remove the most outlier-y points
 cleaned_data = []
+
 try:
     predictions = reg.predict(ages_train)
     cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
 except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
-
-
-
-
-
 
 
 ### only run this code if cleaned_data is returning data
@@ -68,6 +64,10 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "coefficieents:", reg.coef_[0][0]
+        print "intercept:", reg.intercept_[0]
+        print "score:", reg.score(ages_train, net_worths_train)
+        print "score:", reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
