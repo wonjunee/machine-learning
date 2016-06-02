3bf42dab0c5264fd727301ceb881aa0cd5a339d7
wj lee <mymamyma@gmail.com>
2016-06-02 13:04:58 -0400

    reg score

--- regression.py
+++ regression.py
@@ -5,6 +5,12 @@ clf.fit([[0,0],[1,1],[2,2]],[0,1,2])
 
 print clf.coef_
 
-clf.fit([0,1,2],[0,1,2])
+### Import LinearRegression, create regression
+### and fit it to the training data
 
-print clf.coef_
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+
+
+print "r-squared score:", reg.score(ages_test, net_worths_train)
