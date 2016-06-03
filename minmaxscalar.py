aad3a7cfbacaf5d4f9725709d6ab4b05558a9f5d
wj lee <mymamyma@gmail.com>
2016-06-03 15:17:20 -0400

    modified import line

--- minmaxscalar.py
+++ minmaxscalar.py
@@ -5,13 +5,14 @@ X_train = np.array([[1., -1., 2.],
 					[2.,  0., 0.],
 					[0.,  1.,-1.]])
 
-from sklearn import preprocessing
-min_max_scaler = preprocessing.MinMaxScaler()
+from sklearn.preprocessing import MinMaxScaler
+
+min_max_scaler = MinMaxScaler()
 X_train_minmax = min_max_scaler.fit_transform(X_train)
 print X_train_minmax
 
 ### Second example
 weights = np.array([[115.], [140.], [175.]])
-scaler = preprocessing.MinMaxScaler()
+scaler = MinMaxScaler()
 rescaled_weight = scaler.fit_transform(weights)
 print rescaled_weight
