48229c7397fffae3dd7977512d82adfc34020b4d
Wonjun Lee <mymamyma@gmail.com>
2016-06-06 02:33:13 -0400

    added remove list

--- ud120-projects/text_learning/vectorize_text.py
+++ ud120-projects/text_learning/vectorize_text.py
@@ -53,7 +53,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            for word in ["sara", "shackleton", "chris", "germani"]:
+            for word in ["sara", "shackleton", "chris", "germani", "sshacklensf"]:
                 if word in words:
                     words = words.replace(word,'')
 
